e847ab82cc7a459dd6656550fc9bb1e2f0187b55
Mikhil Nagarale <13365191+mikhilnagarale@users.noreply.github.com>
2019-01-30 00:30:34 +0530

    Commenting the Println statements

--- solution/sampleTest.go
+++ solution/sampleTest.go
@@ -38,7 +38,7 @@ basePath := "/home/maersk/test_data/git"
 checkFolders := []string{"raw","tmp","final"}
 //Declaring Map to hold Parent-Child relationship
 myVertices := make(map[string]Node)
-fmt.Println(myVertices)
+//fmt.Println(myVertices)
 for i := range checkFolders{
         output := getDirectories(basePath+"/"+checkFolders[i])
         folderPath := basePath+"/"+checkFolders[i]
@@ -46,9 +46,9 @@ for i := range checkFolders{
         schema := checkFolders[i]
         for lineNo := range lines{
         lineSplit := strings.Split(lines[lineNo]," ")
-        fmt.Println(lineSplit[0],len(lineSplit),len(lineSplit[0]))
+        //fmt.Println(lineSplit[0],len(lineSplit),len(lineSplit[0]))
         if len(lineSplit[0])>0{
-                fmt.Println(lineSplit)
+                //fmt.Println(lineSplit)
                 //Adding table Name with Schema in Map myVertices
                 if !(strings.Contains(lineSplit[0],".sql")){
                         tblName := schema+"."+lineSplit[0]
@@ -56,10 +56,10 @@ for i := range checkFolders{
                         }
                 //If the given element is .sql script then add the target table in Map and then add current script as node & then add
                 //target table as a parent to script
-                fmt.Println(lineSplit[0])
+                //fmt.Println(lineSplit[0])
                 if strings.Contains(lineSplit[0],".sql"){
                         tblName := schema+"."+strings.Replace(lineSplit[0],".sql","",1)
-                        fmt.Println(tblName)
+                        //fmt.Println(tblName)
                         sqlScriptName := lineSplit[0]
                         myVertices[tblName]= Node{parentL: map[string]string{},childL: map[string]string{lineSplit[0]:""}}
                 //Adding .sql script as a Node & target table as it's parent
@@ -75,15 +75,15 @@ for i := range checkFolders{
                                 for splitIndex := range fileLineSplits{
                                         if strings.Contains(fileLineSplits[splitIndex],"raw.")||strings.Contains(fileLineSplits[splitIndex],"final.")||strings.Contains(fileLineSplits[splitIndex],"tmp.")              {
                                                 childTbl := strings.Trim(fileLineSplits[splitIndex],"`")
-                                                fmt.Println(sqlScriptName+"<-"+childTbl)
+                                                //fmt.Println(sqlScriptName+"<-"+childTbl)
                                                 //Add the identified child table to the childL of curerent script
                                                 //statement myVertices[lineSplit[8]]  will return the Node. We need to add childTbl to the childL map of this Node
                                                 myVertices[sqlScriptName].childL[childTbl]=""
                                                 //Add the current script to the parentL of the childTbl. Since We've already added all the tables from rsw schema
                                                 //& then added all the target tables for script then all the child must be present in the myVertices.
-                                                fmt.Println(sqlScriptName)
-                                                fmt.Println(myVertices[childTbl])
-                                                fmt.Println(childTbl)
+                                                //fmt.Println(sqlScriptName)
+                                                //fmt.Println(myVertices[childTbl])
+                                                //fmt.Println(childTbl)
                                                 myVertices[childTbl].parentL[sqlScriptName]=""
                                                 }
                                         }
