642d542bc9de79f5ee243a08b00634b3f1149c6b
Mikhil Nagarale <13365191+mikhilnagarale@users.noreply.github.com>
2019-01-29 17:38:16 +0530

    Updated script for Parent ,Child Data Type of Node
    Updated & tested working -
    In Node parent & child  data type is updated to Map instead of array.
    This will be useful when there of multiple occurrences of same table in script like self join.

--- solution/sampleTest.go
+++ solution/sampleTest.go
@@ -10,8 +10,8 @@ import ("fmt"
 //L at the end indicates
 type Node struct{
         name string
-        parentL []string
-        childL  []string
+        parentL  map[string]string
+        childL   map[string]string
 }
 
 func check(e error){
@@ -21,7 +21,7 @@ func check(e error){
 }
 
 func getDirectories(path string)string{
-out,err:=exec.Command("ls","-lrt",path).Output()
+out,err:=exec.Command("ls",path).Output()
 check(err)
 return string(out)
 }
@@ -37,7 +37,7 @@ func main(){
 basePath := "/home/maersk/test_data/git"
 checkFolders := []string{"raw","tmp","final"}
 //Declaring Map to hold Parent-Child relationship
-myVertices := map[string]Node{}
+myVertices := make(map[string]Node)
 fmt.Println(myVertices)
 for i := range checkFolders{
         output := getDirectories(basePath+"/"+checkFolders[i])
@@ -46,22 +46,26 @@ for i := range checkFolders{
         schema := checkFolders[i]
         for lineNo := range lines{
         lineSplit := strings.Split(lines[lineNo]," ")
-        if len(lineSplit)==9{
-                //fmt.Println(lineSplit)
+        fmt.Println(lineSplit[0],len(lineSplit),len(lineSplit[0]))
+        if len(lineSplit[0])>0{
+                fmt.Println(lineSplit)
                 //Adding table Name with Schema in Map myVertices
-                if !(strings.Contains(lineSplit[8],".sql")){
-                        tblName := schema+"."+lineSplit[8]
-                        myVertices[tblName]= Node{parentL:[]string{},childL:[]string{}}
+                if !(strings.Contains(lineSplit[0],".sql")){
+                        tblName := schema+"."+lineSplit[0]
+                        myVertices[tblName]= Node{parentL: map[string]string{},childL: map[string]string{}}
                         }
                 //If the given element is .sql script then add the target table in Map and then add current script as node & then add
                 //target table as a parent to script
-                if strings.Contains(lineSplit[8],".sql"){
-                        tblName := schema+"."+strings.Trim(lineSplit[8],".sql")
-                        myVertices[tblName]= Node{parentL:[]string{},childL:[]string{lineSplit[8]}}
+                fmt.Println(lineSplit[0])
+                if strings.Contains(lineSplit[0],".sql"){
+                        tblName := schema+"."+strings.Replace(lineSplit[0],".sql","",1)
+                        fmt.Println(tblName)
+                        sqlScriptName := lineSplit[0]
+                        myVertices[tblName]= Node{parentL: map[string]string{},childL: map[string]string{lineSplit[0]:""}}
                 //Adding .sql script as a Node & target table as it's parent
-                        myVertices[lineSplit[8]]= Node{parentL:[]string{tblName},childL:[]string{}}
+                        myVertices[sqlScriptName]= Node{parentL:map[string]string{tblName:""},childL:map[string]string{}}
                 //Read the script Line by Line & add the dependencies accordingly : getFileData
-                        filePath := folderPath+"/"+lineSplit[8]
+                        filePath := folderPath+"/"+lineSplit[0]
                         //fmt.Println(filePath)
                         fileData := getFileData(filePath)
                         //fmt.Println(fileData)
@@ -71,8 +75,16 @@ for i := range checkFolders{
                                 for splitIndex := range fileLineSplits{
                                         if strings.Contains(fileLineSplits[splitIndex],"raw.")||strings.Contains(fileLineSplits[splitIndex],"final.")||strings.Contains(fileLineSplits[splitIndex],"tmp.")              {
                                                 childTbl := strings.Trim(fileLineSplits[splitIndex],"`")
-                                                fmt.Println(lineSplit[8]+"<-"+childTbl)
-                                                //Add the identified
+                                                fmt.Println(sqlScriptName+"<-"+childTbl)
+                                                //Add the identified child table to the childL of curerent script
+                                                //statement myVertices[lineSplit[8]]  will return the Node. We need to add childTbl to the childL map of this Node
+                                                myVertices[sqlScriptName].childL[childTbl]=""
+                                                //Add the current script to the parentL of the childTbl. Since We've already added all the tables from rsw schema
+                                                //& then added all the target tables for script then all the child must be present in the myVertices.
+                                                fmt.Println(sqlScriptName)
+                                                fmt.Println(myVertices[childTbl])
+                                                fmt.Println(childTbl)
+                                                myVertices[childTbl].parentL[sqlScriptName]=""
                                                 }
                                         }
                                 }
